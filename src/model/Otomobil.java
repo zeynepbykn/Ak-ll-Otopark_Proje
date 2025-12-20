9080774e84a508fac94bb2890b9a8b52bc74b0e5
Sudeozbay <24181616031@gazi.edu.tr>
2025-12-21 01:05:56 +0300

    Kontroller yorum satırları açıklamaları

--- src/model/Otomobil.java
+++ src/model/Otomobil.java
@@ -1,12 +1,14 @@
 package model;
 
+//Miras almayi saglamak icin extends
 public class Otomobil extends Arac {
 
+//plakayi ata sinifa gonderiyor plaka cagirmali cunku ata sinif constructorı plaka istiyor.
     public Otomobil(String plaka) {
         super(plaka);
     }
 
-    //abstract metod olan yerKaplamaDurumu metodunu override etmek zorundaydik..
+    //abstract metod olan yerKaplamaDurumu metodunu override etmek zorundaydik.
     @Override
     public String yerKaplamaDurumu() {
         return "Binek arac(2 birim)";
@@ -15,15 +17,12 @@ public class Otomobil extends Arac {
     //Arac sinifinin zorunlu metodu(fiyat hesaplama)
     @Override
     public double odenecekTutar(double sureDakika) {
-//Ornek tarife : Saati 20TL;
         Double saatlikUcret = 20.0;
 
         double normalTutar = (sureDakika / 60.0) * saatlikUcret;
-
         double indirim = normalTutar * indirimOrani();
 
         System.out.println("Hesaplama: Otomobil tarifesi (Saati 20 TL) uygulandı.");
         return normalTutar - indirim;
-
     }
 }
