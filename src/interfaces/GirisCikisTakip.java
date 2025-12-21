845de8bfee13fbdaaad8e53ebb5085facfbbacf8
Sudeozbay <24181616031@gazi.edu.tr>
2025-12-21 13:18:17 +0300

    Dosya Kontrol

--- src/interfaces/GirisCikisTakip.java
+++ src/interfaces/GirisCikisTakip.java
@@ -1,14 +1,8 @@
 package interfaces;
-/*
- * Bu interface, araçların giriş ve çıkış işlemlerini takip etmek için kullanılacaktır.
- */
+
 public interface GirisCikisTakip {
-    /*
-     * Aracın otoparka giriş işlemini gerçekleştirir.
-     */
+
     void girisYap();
-    /*
-     * Aracın otoparktan çıkış işlemini gerçekleştirir.
-     */
+
     void cikisYap();
 }
